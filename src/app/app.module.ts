6256abe31e006779948a375585b36c9bf634968e
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-24 18:46:46 +0300

    lesson 30-31 ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {StockInventoryModule} from "./stock-inventory/stock-inventory.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StockInventoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'stock-inventory',
+  styleUrls: ['stock-inventory.component.scss'],
+  template: `
+  <div class="stock-inventory">
+    <form [formGroup]="form" (ngSubmit)="onSubmit">
+      
+      <div formGroupName="store">
+        <input 
+          type="text" 
+          placeholder="Branch ID"
+          formControlName="branch"
+        >
+        <input 
+          type="text" 
+          placeholder="Manager ID"
+          formControlName="code">
+      </div>
+      
+      <div class="stock-inventory__buttons">
+        <button 
+          type="submit"
+          [disabled]="form.invalid"
+        >
+          Order Stock
+        </button>
+      </div>
+      
+      <pre>
+        {{form.value | json}}
+      </pre>
+      
+    </form>
+  </div>
+  `
+})
+
+export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B128'),
+      code: new FormControl('1234')
+    })
+  });
+
+  onSubmit() {
+    console.log('submit:', this.form.value);
+  }
+}
