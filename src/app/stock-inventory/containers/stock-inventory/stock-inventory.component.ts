ec2cae1942319c09e2906a00a8317176e4f1c284
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-25 16:34:19 +0300

    lesson 32-33 Binding FormGroups to select

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
+import { Product } from '../../models/product.interface'
 
 @Component({
   selector: 'stock-inventory',
@@ -8,17 +9,9 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
   <div class="stock-inventory">
     <form [formGroup]="form" (ngSubmit)="onSubmit">
       
-      <div formGroupName="store">
-        <input 
-          type="text" 
-          placeholder="Branch ID"
-          formControlName="branch"
-        >
-        <input 
-          type="text" 
-          placeholder="Manager ID"
-          formControlName="code">
-      </div>
+      <stock-branch [parent]="form"></stock-branch>
+      <stock-selector [parent]="form" [products]="products"></stock-selector>
+      <stock-products [parent]="form"></stock-products>
       
       <div class="stock-inventory__buttons">
         <button 
@@ -39,11 +32,25 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
 })
 
 export class StockInventoryComponent {
+
+  products: Product[] = [
+    { "id": 1, "price": 2800, "name": "mac book pro"},
+    { "id": 2, "price": 200, "name": "iPad"},
+    { "id": 3, "price": 400, "name": "iPhone"},
+    { "id": 4, "price": 2850, "name": "3mac book pro"},
+    { "id": 5, "price": 300, "name": "7mac book pro"}
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
-      branch: new FormControl('B128'),
-      code: new FormControl('1234')
-    })
+      branch: new FormControl(''),
+      code: new FormControl('')
+    }),
+    selector: new FormGroup({
+      product_id: new FormControl(''),
+      quantity: new FormControl(10)
+    }),
+    stock: new FormArray([])
   });
 
   onSubmit() {
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -3,10 +3,17 @@ import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
 import {StockInventoryComponent} from "./containers/stock-inventory/stock-inventory.component";
 
+import {StockBranchComponent} from "./components/stock-branch/stock-branch.component";
+import {StockSelectorComponent} from "./components/stock-selector/stock-selector.component";
+import {StockProductsComponent} from "./components/stock-products/stock-products.component";
+
 
 @NgModule({
   declarations: [
-    StockInventoryComponent
+    StockInventoryComponent,
+    StockProductsComponent,
+    StockSelectorComponent,
+    StockBranchComponent
   ],
   imports: [
     CommonModule,
