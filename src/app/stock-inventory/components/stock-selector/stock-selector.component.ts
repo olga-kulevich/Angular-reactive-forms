84ab0ca429142a42e7124fe6f0a7d699ca66ca04
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-29 12:46:36 +0300

    lesson 42 ControlValueAccessor

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -16,21 +16,21 @@ import { Product } from '../../models/product.interface'
             {{product.name}}
           </option>
         </select>
-        
+      <!--
         <input
           type="number"
           step="10"
           min="10"
           max="1000"
-          formControlName="quantity"
-        >
+          formControlName="quantity">
+      -->
         
         <stock-counter
           [step]="10"
           [min]="10"
           [max]="1000"
+          formControlName="quantity"
         >
-          
         </stock-counter>
         
         <button type="submit"
