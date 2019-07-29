d289aee3accffc9866423c76f951b378f9bfb8ec
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-29 16:13:15 +0300

    lesson 44 Validators object for FormControls

--- src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
+++ src/app/stock-inventory/components/stock-branch/stock-branch.component.ts
@@ -10,12 +10,23 @@ import {FormGroup} from "@angular/forms";
         <input
           type="text"
           placeholder="Branch ID"
-          formControlName="branch"
-        >
+          formControlName="branch">
+        <div
+          class="error"
+          *ngIf="parent.get('store.branch').hasError('required') && parent.get('store.branch').touched">
+          Branch ID is Required
+        </div>
+        
         <input
           type="text"
           placeholder="Manager ID"
           formControlName="code">
+        <div 
+          class="error" 
+          *ngIf="parent.get('store.code').hasError('required') && parent.get('store.code').touched">
+          Manager ID is Required
+        </div>
+        
       </div>
     </div>
   `
@@ -24,4 +35,13 @@ import {FormGroup} from "@angular/forms";
 export class StockBranchComponent {
   @Input()
   parent: FormGroup;
+
+  /*
+  required(name: string) {
+    return (
+      this.parent.get(`store.${name}`).hasError('required') &&
+      this.parent.get(`store.${name}`).touched
+    )
+  }*/
+
 }
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
+import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 import { Product, Item } from '../../models/product.interface';
 
 import {forkJoin} from "rxjs";
@@ -60,8 +60,8 @@ export class StockInventoryComponent implements OnInit{
 
   form = this.fb.group({
     store: this.fb.group({
-      branch: '',
-      code: ''
+      branch: ['', Validators.required],
+      code: ['', Validators.required]
     }),
     selector: this.createStock({}),
     stock: this.fb.array([])
